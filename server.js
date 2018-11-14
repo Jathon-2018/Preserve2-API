68b32de852941fbb7075440e2442f7bca2573e50
apasiri <apasiri.du1996@gmail.com>
2018-11-14 22:26:20 +0700

    fristcommit

--- server.js
+++ server.js
@@ -1,16 +1,53 @@
-const express = require('express')
-const app = express()
- 
+var app = require('express')();
+var bodyParser = require('body-parser');
+require('dotenv').config();
+var mysql = require('mysql');
+var fuctions = require('./fuctions.js');
+
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8100');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
+
+var port = process.env.PORT || 4000;
+//parse
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
+
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.send('!!!!!!!!!')
+  console.log("homeStart")
 })
 
+
+
 app.post('/Login',function(req,res){
-    // console.log(req.body.username)
-    appFuctions.Login(req,res);
+    console.log(req.body.username)
+    fuctions.Login(req,res);
    
   });
  
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+
+
+
+// port connect server 
+app.listen(port,"0.0.0.0",function () {
+  console.log("Listening on Port "+port);
 })
